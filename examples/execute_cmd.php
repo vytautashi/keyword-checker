ed67afc75f373ccb81c363b46443d0b926a2fc83
vytautashi <wbogusas@yahoo.com>
2021-08-24 23:21:41 +0300

    closes #2, add example

---
+++ examples/execute_cmd.php
@@ -0,0 +1,42 @@
+<?php
+// This example runs script via command and
+// finds website positions in search engine
+// for specific keywords.
+
+const WEBSITE = 'https://golang.org/';
+const KEYWORDS = 'golang,google programming,design';
+
+main();
+
+function main()
+{
+    $command = 'php ../keyword_checker.php "' . KEYWORDS . '"';
+    exec($command, $output, $retval);
+
+    if ($retval !== 0) {
+        exit('ERROR: ' . implode("\n", $output));
+    }
+
+    $json = implode("", $output);
+    $rankings = json_decode($json);
+
+    print_r(find_positions($rankings, WEBSITE));
+}
+
+function find_positions($data, $website)
+{
+    $keywords_pos = [];
+
+    foreach ($data as $item) {
+        $i = 1;
+        $pos = [];
+        foreach ($item->positions as $web) {
+            if ($web === $website) {
+                $pos[] = $i;
+            }
+            $i++;
+        }
+        $keywords_pos[$item->keyword] = implode(',', $pos);
+    }
+    return $keywords_pos;
+}
